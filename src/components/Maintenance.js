66388fce2ba24cfe5afc48f1dc5e3be1cb8273a1
whphhg <whphhg@protonmail.ch>
2016-09-21 21:48:10 +0200

    Select currency antd port

--- src/components/Maintenance.js
+++ src/components/Maintenance.js
@@ -1,45 +1,43 @@
 import React from 'react'
 import { inject, observer } from 'mobx-react'
+import { Col, Row, Select } from 'antd'
 
-import MenuItem from 'material-ui/MenuItem'
-import SelectField from 'material-ui/SelectField'
-
-@inject('rates')
-@observer
+/** Make the component reactive and inject MobX stores. */
+@observer(['rates'])
 
 class Maintenance extends React.Component {
   constructor(props) {
     super(props)
     this.rates = props.rates
+
+    /** Bind functions early. */
     this.setLocalCurrency = this.setLocalCurrency.bind(this)
   }
 
-  setLocalCurrency(event, index, value) {
+  setLocalCurrency(value) {
     this.rates.setLocalCurrency(value)
   }
 
   render() {
     return (
-      <div className='container-fluid'>
-        <div className='row'>
-          <div className='col-md-12'>
-            <h5>Maintenance</h5>
-            <SelectField
-              autoWidth={true}
-              style={{width:'150px'}}
-              value={this.rates.localCurrency}
-              onChange={this.setLocalCurrency}
-              floatingLabelText='Set local currency'
-            >
-              {
-                this.rates.localCurrencies.map((currency) => (
-                  <MenuItem key={currency} value={currency} primaryText={currency} />
-                ))
-              }
-            </SelectField>
-          </div>
-        </div>
-      </div>
+      <Row>
+        <Col span={4}>
+          <p>Set local currency</p>
+          <Select showSearch
+            style={{ width: 200 }}
+            placeholder={this.rates.localCurrency}
+            optionFilterProp='children'
+            notFoundContent='Currency not found.'
+            onChange={this.setLocalCurrency}
+          >
+            {
+              this.rates.localCurrencies.map((currency) => (
+                <Select.Option key={currency} value={currency}>{currency}</Select.Option>
+              ))
+            }
+          </Select>
+        </Col>
+      </Row>
     )
   }
 }
