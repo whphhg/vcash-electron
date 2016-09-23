2f4255ad042e663ce92adc66d638611e5b0d975e
whphhg <whphhg@protonmail.ch>
2016-09-23 23:11:54 +0200

    RewardCalculator antd port

--- src/components/RewardCalculator.js
+++ src/components/RewardCalculator.js
@@ -1,77 +1,85 @@
 import React from 'react'
 import { inject, observer } from 'mobx-react'
+import { Button, Col, Input, Popover, Row } from 'antd'
 import moment from 'moment'
-import TextField from 'material-ui/TextField'
 
-@inject('rewardCalculator')
-@inject('wallet')
-@observer
+/** Required components. */
+import RewardCalculatorChart from './RewardCalculatorChart'
+
+/** Make the component reactive and inject MobX stores. */
+@observer(['rewardCalculator', 'wallet'])
 
 class RewardCalculator extends React.Component {
   constructor(props) {
     super(props)
     this.rewardCalculator = props.rewardCalculator
     this.wallet = props.wallet
-    this.onChange = this.onChange.bind(this)
 
     /** Calculate current block when loading component. */
     this.rewardCalculator.setBlock(this.wallet.blocks)
+
+    /** Bind functions early. */
+    this.onChange = this.onChange.bind(this)
   }
 
   onChange(event) {
-    const block = event.target.value
-    this.rewardCalculator.setBlock(block)
+    this.rewardCalculator.setBlock(event.target.value)
   }
 
   render() {
     return (
-      <div>
-        <TextField
-          value={this.rewardCalculator.block}
-          onChange={this.onChange}
-          maxLength={7}
-          hintText='Enter block number'
-          floatingLabelStyle={{fontWeight:'normal'}}
-          floatingLabelText='Calculate block rewards'
-          fullWidth={true}
-        />
-        {
-          this.rewardCalculator.block &&
-          (
-            <div>
-              <div className='row'>
-                <div className='col-md-12'>
-                  <p>
-                    {this.rewardCalculator.estimate ? 'Confirmation est.' : 'Confirmed on'}
-                    <span className='font-weight-500'> {moment(this.rewardCalculator.time).format('YYYY-MM-DD HH:mm:ss')} </span>
-                    (<span className='font-weight-500'>{moment().to(this.rewardCalculator.time.toISOString())}</span>)
-                  </p>
-                </div>
-              </div>
-
-              <div className='row'>
-                <div className='col-md-5 noPadding noMargin'>
-                  <p>PoW reward</p>
-                  <p>Miner share</p>
-                  <p>Incentive share</p>
-                </div>
-
-                <div className='col-md-5 noPadding noMargin textRight'>
-                  <p><span className='font-weight-500'>{this.rewardCalculator.powReward}</span> XVC</p>
-                  <p><span className='font-weight-500'>{(this.rewardCalculator.powReward - this.rewardCalculator.incentiveReward).toFixed(6)}</span> XVC</p>
-                  <p><span className='font-weight-500'>{this.rewardCalculator.incentiveReward}</span> XVC</p>
-                </div>
-
-                <div className='col-md-2 noPadding noMargin'>
-                  <p>&nbsp;</p>
-                  <p><span className='font-weight-500'>{100 - this.rewardCalculator.powPercent}</span>%</p>
-                  <p><span className='font-weight-500'>{this.rewardCalculator.powPercent}</span>%</p>
-                </div>
-              </div>
-            </div>
-          )
+      <Popover
+        trigger='click'
+        placement='bottomRight'
+        title={
+          <Row>
+            <Col span={2}>
+              <span>Block</span>
+            </Col>
+            <Col span={4}>
+              <Input type='text' autosize
+                placeholder='Enter block number'
+                value={this.rewardCalculator.block}
+                onChange={this.onChange}
+                maxLength={7}
+              />
+            </Col>
+            <Col span={18}>
+              <p style={{textAlign:'right'}}>
+                {this.rewardCalculator.estimate ? 'Confirmation est. ' : 'Confirmed on '}
+                <span className='font-weight-500'>{moment(this.rewardCalculator.time).format('YYYY-MM-DD HH:mm:ss')}</span> ({moment().to(this.rewardCalculator.time.toISOString())})
+              </p>
+            </Col>
+          </Row>
         }
-      </div>
+        content={
+          <Row style={{width:'500px', marginTop:'10px'}}>
+            <Col offset={6} span={6}>
+              <p>PoW reward</p>
+              <p>Miner share</p>
+              <p>Incentive share</p>
+            </Col>
+
+            <Col span={6}>
+              <p><span className='font-weight-500'>{this.rewardCalculator.powReward}</span> XVC</p>
+              <p><span className='font-weight-500'>{(this.rewardCalculator.powReward - this.rewardCalculator.incentiveReward).toFixed(6)}</span> XVC</p>
+              <p><span className='font-weight-500'>{this.rewardCalculator.incentiveReward}</span> XVC</p>
+            </Col>
+
+            <Col span={2}>
+              <p>&nbsp;</p>
+              <p><span className='font-weight-500'>{100 - this.rewardCalculator.powPercent}</span>%</p>
+              <p><span className='font-weight-500'>{this.rewardCalculator.powPercent}</span>%</p>
+            </Col>
+
+            <Col span={24}>
+              <RewardCalculatorChart />
+            </Col>
+          </Row>
+        }
+      >
+        <Button>Reward calculator</Button>
+      </Popover>
     )
   }
 }
--- src/components/RewardCalculatorChart.js
+++ src/components/RewardCalculatorChart.js
@@ -2,8 +2,8 @@ import React from 'react'
 import { inject, observer } from 'mobx-react'
 import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip } from 'recharts'
 
-@inject('rewardCalculator')
-@observer
+/** Make the component reactive and inject MobX stores. */
+@observer(['rewardCalculator'])
 
 class RewardCalculatorChart extends React.Component {
   constructor(props) {
@@ -13,9 +13,9 @@ class RewardCalculatorChart extends React.Component {
 
   render() {
     return (
-      <LineChart width={730} height={230} data={this.rewardCalculator.chartData} margin={{top:20, right:25}}>
-        <CartesianGrid stroke='#ccc' strokeDasharray='6 6' />
-        <Line type='monotone' dataKey='PoW reward' stroke='#b60127' />
+      <LineChart width={480} height={230} data={this.rewardCalculator.chartData} margin={{top:20, right:25}}>
+        <CartesianGrid stroke='#CCCCCC' strokeDasharray='5 5' />
+        <Line type='monotone' dataKey='PoW reward' stroke='#B60127' />
         <Line type='monotone' dataKey='Miner share' stroke='#1A237E' />
         <Line type='monotone' dataKey='Incentive share' stroke='#33691E' />
         <Tooltip />
