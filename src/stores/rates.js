d175fbdd1829adb6098a5a7d6510e35d950a25ff
whphhg <whphhg@protonmail.ch>
2016-11-09 17:13:42 +0100

    Overhauls 7

--- src/stores/rates.js
+++ src/stores/rates.js
@@ -3,78 +3,32 @@ import { getItem, setItem } from '../utilities/localStorage'
 
 /** Rates store class. */
 class Rates {
-  @observable bitcoinAverage
-  @observable bittrex
   @observable localCurrency
+  @observable bitcoinAverage
   @observable poloniex
+  @observable bittrex
   @observable rawx
 
   /**
-   * Prepare observable variables and run fetching functions.
    * @constructor
-   * @property {string} localCurrency - User selected local currency.
+   * @property {string} localCurrency - Selected local currency.
    * @property {object} bitcoinAverage - Bitcoin average price index.
-   * @property {object} bittrex - Bittrex ticker.
    * @property {object} poloniex - Poloniex ticker.
+   * @property {object} bittrex - Bittrex ticker.
    * @property {object} rawx - Rawx ticker.
    */
   constructor() {
     this.localCurrency = getItem('localCurrency') || 'EUR'
     this.bitcoinAverage = {}
-    this.bittrex = { Last: 0 }
     this.poloniex = { last: 0 }
+    this.bittrex = { Last: 0 }
     this.rawx = { lastprice: 0 }
 
+    /** Start rates update loops. */
     this.fetchBitcoinAverage()
-    this.fetchBittrex()
     this.fetchPoloniex()
+    this.fetchBittrex()
     this.fetchRawx()
-  }
-
-  /**
-   * Set bitcoin average price index.
-   * @function setBitcoinAverage
-   * @param {string} priceIndex - New price index.
-   */
-  @action setBitcoinAverage(priceIndex) {
-    this.bitcoinAverage = priceIndex
-  }
-
-  /**
-   * Set Bittrex ticker.
-   * @function setBittrex
-   * @param {string} ticker - Bittrex ticker.
-   */
-  @action setBittrex(ticker) {
-    this.bittrex = ticker
-  }
-
-  /**
-   * Set Poloniex ticker.
-   * @function setPoloniex
-   * @param {string} ticker - Poloniex ticker.
-   */
-  @action setPoloniex(ticker) {
-    this.poloniex = ticker
-  }
-
-  /**
-   * Set Rawx ticker.
-   * @function setRawx
-   * @param {string} ticker - Rawx ticker.
-   */
-  @action setRawx(ticker) {
-    this.rawx = ticker
-  }
-
-  /**
-   * Set provided local currency and save to localStorage.
-   * @function setLocalCurrency
-   * @param {string} localCurrency - Provided local currency.
-   */
-  @action setLocalCurrency(localCurrency) {
-    this.localCurrency = localCurrency
-    setItem('localCurrency', localCurrency)
   }
 
   /**
@@ -84,21 +38,19 @@ class Rates {
    */
   @computed get average() {
     const rates = [this.poloniex.last, this.bittrex.Last, this.rawx.lastprice]
-    let total = 0
-    let divideBy = 0
+    const result = rates.reduce((result, rate) => {
+      rate = parseFloat(rate)
 
-    rates.map((rate) => {
       if (rate > 0) {
-        total += parseFloat(rate)
-        divideBy += 1
+        result.total += rate
+        result.divideBy += 1
       }
-    })
 
-    if (isNaN(total / divideBy)) {
-      return 0
-    }
+      return result
+    }, { total: 0, divideBy: 0 })
 
-    return parseFloat(total / divideBy)
+    if (result.total > 0) return result.total / result.divideBy
+    return 0
   }
 
   /**
@@ -107,11 +59,8 @@ class Rates {
    * @return {number} Local bitcoin price.
    */
   @computed get local() {
-    if (this.localCurrency in this.bitcoinAverage) {
-      return this.bitcoinAverage[this.localCurrency].last
-    } else {
-      return 0
-    }
+    if (this.localCurrency in this.bitcoinAverage === true) return this.bitcoinAverage[this.localCurrency].last
+    return 0
   }
 
   /**
@@ -119,37 +68,75 @@ class Rates {
    * @function localCurrencies
    * @return {array} Local currencies.
    */
-  @computed get localCurrencies() {
-    return Object.keys(this.bitcoinAverage)
+  @computed get localCurrencies() { return Object.keys(this.bitcoinAverage) }
+
+  /**
+   * Set bitcoin average price index.
+   * @function setBitcoinAverage
+   * @param {string} priceIndex - Price index.
+   */
+  @action setBitcoinAverage(priceIndex) {
+    delete priceIndex.timestamp
+    this.bitcoinAverage = priceIndex
   }
 
   /**
-   * Fetch bitcoin average price index.
+   * Set Poloniex ticker.
+   * @function setPoloniex
+   * @param {string} ticker - Ticker.
+   */
+  @action setPoloniex(ticker) { this.poloniex = ticker['BTC_XVC'] }
+
+  /**
+   * Set Bittrex ticker.
+   * @function setBittrex
+   * @param {string} ticker - Ticker.
+   */
+  @action setBittrex(ticker) { this.bittrex = ticker.result[0] }
+
+  /**
+   * Set Rawx ticker.
+   * @function setRawx
+   * @param {string} ticker - Ticker.
+   */
+  @action setRawx(ticker) {
+    for (let i in ticker.pairs) {
+      if (ticker.pairs[i].other === 'XVC') {
+        const internalDecimals = Math.pow(10, 12)
+
+        ticker.pairs[i].lastprice = parseInt('0x' + ticker.pairs[i].lastprice) / internalDecimals
+        ticker.pairs[i].volume = parseInt('0x' + ticker.pairs[i].volume) / internalDecimals
+        ticker.pairs[i].bestask = parseInt('0x' + ticker.pairs[i].bestask) / internalDecimals
+        ticker.pairs[i].bestbid = parseInt('0x' + ticker.pairs[i].bestbid) / internalDecimals
+        ticker.pairs[i].minbase = parseInt('0x' + ticker.pairs[i].minbase) / internalDecimals
+        ticker.pairs[i].depthbid = parseInt('0x' + ticker.pairs[i].depthbid) / internalDecimals
+
+        this.rawx = ticker.pairs[i]
+        break
+      }
+    }
+  }
+
+  /**
+   * Set local currency and save to localStorage.
+   * @function setLocalCurrency
+   * @param {string} localCurrency - Local currency.
+   */
+  @action setLocalCurrency(localCurrency) {
+    this.localCurrency = localCurrency
+    setItem('localCurrency', localCurrency)
+  }
+
+  /**
+   * Fetch BitcoinAverage price index.
    * @function fetchBitcoinAverage
    */
   fetchBitcoinAverage() {
     fetch('https://api.bitcoinaverage.com/ticker/global/all')
       .then((response) => { if (response.ok) return response.json() })
-      .then((priceIndex) => {
-        delete priceIndex.timestamp
-        this.setBitcoinAverage(priceIndex)
-      })
+      .then((priceIndex) => { this.setBitcoinAverage(priceIndex) })
       .catch((error) => { process.env.NODE_ENV === 'dev' && console.error('https://api.bitcoinaverage.com/ticker/global/all:', error.message) })
-
     setTimeout(() => { this.fetchBitcoinAverage() }, 120 * 1000)
-  }
-
-  /**
-   * Fetch Bittrex ticker.
-   * @function fetchBittrex
-   */
-  fetchBittrex() {
-    fetch('https://bittrex.com/api/v1.1/public/getmarketsummary?market=btc-xvc')
-      .then((response) => { if (response.ok) return response.json() })
-      .then((ticker) => { this.setBittrex(ticker.result[0]) })
-      .catch((error) => { process.env.NODE_ENV === 'dev' && console.error('https://bittrex.com/api/v1.1/public/getmarketsummary?market=btc-xvc:', error.message) })
-
-    setTimeout(() => { this.fetchBittrex() }, 60 * 1000)
   }
 
   /**
@@ -159,10 +146,21 @@ class Rates {
   fetchPoloniex() {
     fetch('https://poloniex.com/public?command=returnTicker')
       .then((response) => { if (response.ok) return response.json() })
-      .then((ticker) => { this.setPoloniex(ticker['BTC_XVC']) })
+      .then((ticker) => { this.setPoloniex(ticker) })
       .catch((error) => { process.env.NODE_ENV === 'dev' && console.error('https://poloniex.com/public?command=returnTicker:', error.message) })
-
     setTimeout(() => { this.fetchPoloniex() }, 30 * 1000)
+  }
+
+  /**
+   * Fetch Bittrex ticker.
+   * @function fetchBittrex
+   */
+  fetchBittrex() {
+    fetch('https://bittrex.com/api/v1.1/public/getmarketsummary?market=btc-xvc')
+      .then((response) => { if (response.ok) return response.json() })
+      .then((ticker) => { this.setBittrex(ticker) })
+      .catch((error) => { process.env.NODE_ENV === 'dev' && console.error('https://bittrex.com/api/v1.1/public/getmarketsummary?market=btc-xvc:', error.message) })
+    setTimeout(() => { this.fetchBittrex() }, 60 * 1000)
   }
 
   /**
@@ -172,30 +170,15 @@ class Rates {
   fetchRawx() {
     fetch('https://beta.rawx.io/m')
       .then((response) => { if (response.ok) return response.json() })
-      .then((ticker) => {
-        for (let i in ticker.pairs) {
-          if (ticker.pairs[i].other === 'XVC') {
-            const internalDecimals = Math.pow(10, 12)
-
-            ticker.pairs[i].lastprice = parseInt('0x' + ticker.pairs[i].lastprice) / internalDecimals
-            ticker.pairs[i].volume = parseInt('0x' + ticker.pairs[i].volume) / internalDecimals
-            ticker.pairs[i].bestask = parseInt('0x' + ticker.pairs[i].bestask) / internalDecimals
-            ticker.pairs[i].bestbid = parseInt('0x' + ticker.pairs[i].bestbid) / internalDecimals
-            ticker.pairs[i].minbase = parseInt('0x' + ticker.pairs[i].minbase) / internalDecimals
-            ticker.pairs[i].depthbid = parseInt('0x' + ticker.pairs[i].depthbid) / internalDecimals
-
-            this.setRawx(ticker.pairs[i])
-            break
-          }
-        }
-      })
+      .then((ticker) => { this.setRawx(ticker) })
       .catch((error) => { process.env.NODE_ENV === 'dev' && console.error('https://beta.rawx.io/m:', error.message) })
-
     setTimeout(() => { this.fetchRawx() }, 60 * 1000)
   }
 }
 
+/** Initialize a new globally used store. */
 const rates = new Rates()
 
+/** Export both, initialized store as default export, and store class as named export. */
 export default rates
 export { Rates }
