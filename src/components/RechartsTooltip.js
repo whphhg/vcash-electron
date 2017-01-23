b3c022006aad76ff4ab52cc11a2fe55ffbf55c47
whphhg <whphhg@protonmail.ch>
2017-01-23 22:05:34 +0100

    Cleanup

--- src/components/RechartsTooltip.js
+++ src/components/RechartsTooltip.js
@@ -1,51 +1,44 @@
 import React from 'react'
-import { inject, observer } from 'mobx-react'
 import { Col, Row } from 'antd'
 
-/** Make the component reactive and inject MobX stores. */
-@inject('ui') @observer
+/** Required stores. */
+import ui from '../stores/ui'
 
-class RechartsTooltip extends React.Component {
-  constructor (props) {
-    super(props)
-    this.ui = props.ui
-  }
-
-  render () {
-    if (this.props.active === false) return null
-    return (
-      <div className='chartTooltip'>
-        <p className='label'>
-          {this.props.labelText} {this.props.label}
-        </p>
-        <Row>
-          {
-            this.props.payload.map((entry) => {
-              return (
-                <div key={entry.color + entry.name}>
-                  <Col span={12}>
-                    <p style={{color: entry.color}}>
-                      {entry.name}
-                    </p>
-                  </Col>
-                  <Col span={12} className='text-right'>
-                    <p style={{color: entry.color}}>
-                      {
-                        new Intl.NumberFormat(this.ui.language, {
-                          minimumFractionDigits: 6,
-                          maximumFractionDigits: 6
-                        }).format(entry.value)
-                      } XVC
-                    </p>
-                  </Col>
-                </div>
-              )
-            })
-          }
-        </Row>
-      </div>
-    )
-  }
+/** Custom Recharts tooltip. */
+const RechartsTooltip = (props) => {
+  if (props.active === false) return null
+  return (
+    <div className='chartTooltip'>
+      <p className='label'>
+        {props.labelText} {props.label}
+      </p>
+      <Row>
+        {
+          props.payload.map((entry) => {
+            return (
+              <div key={entry.color + entry.name}>
+                <Col span={12}>
+                  <p style={{color: entry.color}}>
+                    {entry.name}
+                  </p>
+                </Col>
+                <Col span={12} className='text-right'>
+                  <p style={{color: entry.color}}>
+                    {
+                      new Intl.NumberFormat(ui.language, {
+                        minimumFractionDigits: 6,
+                        maximumFractionDigits: 6
+                      }).format(entry.value)
+                    } XVC
+                  </p>
+                </Col>
+              </div>
+            )
+          })
+        }
+      </Row>
+    </div>
+  )
 }
 
 export default RechartsTooltip
--- src/components/TableCell.js
+++ src/components/TableCell.js
@@ -1,126 +1,112 @@
 import React from 'react'
-import { translate } from 'react-i18next'
-import { inject, observer } from 'mobx-react'
 import { Cell } from 'fixed-data-table'
 import moment from 'moment'
+import i18next from '../utilities/i18next'
 
-/** Load translation namespaces and delay rendering until they are loaded. */
-@translate(['wallet'], { wait: true })
+/** Required stores. */
+import ui from '../stores/ui'
 
-/** Make the component reactive and inject MobX stores. */
-@inject('ui') @observer
+/** Dynamic fixed-data-table cell creation component. */
+const TableCell = ({ rowIndex, data, column, type, extra, ...props }) => {
+  switch (type) {
+    case 'localAmount':
+      return (
+        <Cell
+          {...props}
+          className={'fdt-last-column ' + data[rowIndex]['color']}
+        >
+          {data[rowIndex][column]} {extra}
+        </Cell>
+      )
 
-class TableCell extends React.Component {
-  constructor (props) {
-    super(props)
-    this.t = props.t
-    this.ui = props.ui
-    this.props = props
-  }
+    default:
+      switch (column) {
+        case 'account':
+          switch (data[rowIndex][column]) {
+            case '*':
+              return (
+                <Cell {...props}>
+                  <i>{i18next.t('wallet:multipleAccounts')}</i>
+                </Cell>
+              )
 
-  render () {
-    const { rowIndex, data, column, type, extra } = this.props
+            case '':
+              return (
+                <Cell {...props}>
+                  <i>{i18next.t('wallet:default')}</i>
+                </Cell>
+              )
 
-    switch (type) {
-      case 'localAmount':
-        return (
-          <Cell
-            {...this.props}
-            className={'fdt-last-column ' + data[rowIndex]['color']}
-          >
-            {data[rowIndex][column]} {extra}
-          </Cell>
-        )
+            default:
+              return (
+                <Cell {...props}>
+                  {data[rowIndex][column]}
+                </Cell>
+              )
+          }
 
-      default:
-        switch (column) {
-          case 'account':
-            switch (data[rowIndex][column]) {
-              case '*':
-                return (
-                  <Cell {...this.props}>
-                    <i>{this.t('wallet:multipleAccounts')}</i>
-                  </Cell>
-                )
+        case 'address':
+          return (
+            <Cell {...props} className='text-mono'>
+              {data[rowIndex][column]}
+            </Cell>
+          )
 
-              case '':
-                return (
-                  <Cell {...this.props}>
-                    <i>{this.t('wallet:default')}</i>
-                  </Cell>
-                )
+        case 'amount':
+          return (
+            <Cell
+              {...props}
+              className={'text-right ' + data[rowIndex]['color']}
+            >
+              {
+                new Intl.NumberFormat(ui.language, {
+                  minimumFractionDigits: 6,
+                  maximumFractionDigits: 6
+                }).format(data[rowIndex][column])
+              } XVC
+            </Cell>
+          )
 
-              default:
-                return (
-                  <Cell {...this.props}>
-                    {data[rowIndex][column]}
-                  </Cell>
-                )
-            }
+        case 'banscore':
+          return (
+            <Cell {...props}>
+              {data[rowIndex][column]}/100
+            </Cell>
+          )
 
-          case 'address':
-            return (
-              <Cell {...this.props} className='text-mono'>
-                {data[rowIndex][column]}
-              </Cell>
-            )
+        case 'conntime':
+          return (
+            <Cell {...props}>
+              {moment(data[rowIndex][column]).fromNow()}
+            </Cell>
+          )
 
-          case 'amount':
-            return (
-              <Cell
-                {...this.props}
-                className={'text-right ' + data[rowIndex]['color']}
-              >
-                {
-                  new Intl.NumberFormat(this.ui.language, {
-                    minimumFractionDigits: 6,
-                    maximumFractionDigits: 6
-                  }).format(data[rowIndex][column])
-                } XVC
-              </Cell>
-            )
+        case 'time':
+          return (
+            <Cell {...props}>
+              {moment(data[rowIndex][column]).format('l - HH:mm:ss')}
+            </Cell>
+          )
 
-          case 'banscore':
-            return (
-              <Cell {...this.props}>
-                {data[rowIndex][column]}/100
-              </Cell>
-            )
+        case 'value':
+          return (
+            <Cell {...props}>
+              {
+                new Intl.NumberFormat(ui.language, {
+                  minimumFractionDigits: 6,
+                  maximumFractionDigits: 6
+                }).format(data[rowIndex][column])
+              } XVC
+            </Cell>
+          )
 
-          case 'conntime':
-            return (
-              <Cell {...this.props}>
-                {moment(data[rowIndex][column]).fromNow()}
-              </Cell>
-            )
-
-          case 'time':
-            return (
-              <Cell {...this.props}>
-                {moment(data[rowIndex][column]).format('l - HH:mm:ss')}
-              </Cell>
-            )
-
-          case 'value':
-            return (
-              <Cell {...this.props}>
-                {
-                  new Intl.NumberFormat(this.ui.language, {
-                    minimumFractionDigits: 6,
-                    maximumFractionDigits: 6
-                  }).format(data[rowIndex][column])
-                } XVC
-              </Cell>
-            )
-
-          default:
-            return (
-              <Cell {...this.props}>
-                {data[rowIndex][column]}
-              </Cell>
-            )
-        }
-    }
+        default:
+          return (
+            <Cell {...props}>
+              {data[rowIndex][column]}
+            </Cell>
+          )
+      }
   }
 }
 
