68de8586bee1a07025039cccbc9c0d8ae89a18a3
whphhg <whphhg@protonmail.ch>
2017-02-24 19:44:54 +0100

    ChainBlender: info in 1 line instead of Popover

--- src/components/ChainBlender.js
+++ src/components/ChainBlender.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { translate } from 'react-i18next'
 import { inject, observer } from 'mobx-react'
-import { Button, Col, Popover, Row, Switch } from 'antd'
+import { Switch } from 'antd'
 
 /** Load translation namespaces and delay rendering until they are loaded. */
 @translate(['wallet'], { wait: true })
@@ -9,7 +9,7 @@ import { Button, Col, Popover, Row, Switch } from 'antd'
 /** Make the component reactive and inject MobX stores. */
 @inject('ui', 'wallet') @observer
 
-class ChainBlender extends React.Component {
+export default class ChainBlender extends React.Component {
   constructor (props) {
     super(props)
     this.t = props.t
@@ -22,72 +22,55 @@ class ChainBlender extends React.Component {
     this.wallet.toggleBlender()
   }
 
-  popoverTitle () {
+  render () {
     const { chainBlender, isBlending, isLocked } = this.wallet
 
     return (
-      <Row style={{width: '265px'}}>
-        <Col span={17}>
-          <p>
-            <span>{this.t('wallet:blended')} </span>
-            <span className='text-dotted'>
-              {
-                new Intl.NumberFormat(this.ui.language, {
-                  minimumFractionDigits: 6,
-                  maximumFractionDigits: 6
-                }).format(chainBlender.blendedbalance)
-              }
-            </span>
-            <span> XVC (
-              {
-                new Intl.NumberFormat(this.ui.language, {
-                  minimumFractionDigits: 2,
-                  maximumFractionDigits: 2
-                }).format(chainBlender.blendedpercentage)
-              }%)
-            </span>
-          </p>
-        </Col>
-        <Col span={7} className='text-right'>
-          <Switch
-            checked={isBlending === true}
-            disabled={isLocked === true}
-            onChange={this.toggle}
-            checkedChildren={
-              <i
-                className='material-icons md-18'
-                style={{margin: '1px 0 0 0'}}
-              >
-                done
-              </i>
+      <div className='left'>
+        <Switch
+          size='small'
+          checked={isBlending === true}
+          disabled={isLocked === true}
+          onChange={this.toggle}
+          checkedChildren={
+            <i
+              className='material-icons md-16'
+              style={{margin: '-2px 0 0 0'}}
+            >
+              done
+            </i>
+          }
+          unCheckedChildren={
+            <i
+              className='material-icons md-16'
+              style={{margin: '-2px 0 0 0'}}
+            >
+              clear
+            </i>
+          }
+        />
+        <p style={{margin: '0 0 0 11px'}}>
+          {this.t('wallet:blended')} <span>
+            {
+              new Intl.NumberFormat(this.ui.language, {
+                minimumFractionDigits: 6,
+                maximumFractionDigits: 6
+              }).format(chainBlender.blendedbalance)
             }
-            unCheckedChildren={
-              <i
-                className='material-icons md-18'
-                style={{margin: '1px 0 0 0'}}
-              >
-                clear
-              </i>
+          </span> XVC (
+          <span>
+            {
+              new Intl.NumberFormat(this.ui.language, {
+                minimumFractionDigits: 2,
+                maximumFractionDigits: 2
+              }).format(chainBlender.blendedpercentage)
             }
-          />
-        </Col>
-      </Row>
-    )
-  }
-
-  popoverContent () {
-    const { chainBlender } = this.wallet
-
-    return (
-      this.wallet.isLocked === false && (
-        <Row>
-          <Col span={12}>
-            <p>{this.t('wallet:denominated')}</p>
-            <p>{this.t('wallet:nonDenominated')}</p>
-          </Col>
-          <Col span={12} className='text-right'>
+          </span>%)
+        </p>
+        {
+          this.wallet.isLocked === false && (
             <p>
-              <span className='text-dotted'>
+              {this.t('wallet:denominated')} <span>
                 {
                   new Intl.NumberFormat(this.ui.language, {
                     minimumFractionDigits: 6,
@@ -96,36 +79,9 @@ class ChainBlender extends React.Component {
                 }
               </span> XVC
             </p>
-            <p>
-              <span className='text-dotted'>
-                {
-                  new Intl.NumberFormat(this.ui.language, {
-                    minimumFractionDigits: 6,
-                    maximumFractionDigits: 6
-                  }).format(chainBlender.nondenominatedbalance)
-                }
-              </span> XVC
-            </p>
-          </Col>
-        </Row>
-      ) || (
-        <p>{this.t('wallet:unlockRequired')}</p>
-      )
-    )
-  }
-
-  render () {
-    return (
-      <Popover
-        trigger='click'
-        placement='bottomLeft'
-        title={this.popoverTitle()}
-        content={this.popoverContent()}
-      >
-        <Button>ChainBlender</Button>
-      </Popover>
+          )
+        }
+      </div>
     )
   }
 }
-
-export default ChainBlender
