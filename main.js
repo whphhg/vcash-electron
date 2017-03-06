c66093f0fc66a508a59506b71a03384f25a0848b
whphhg <whphhg@protonmail.ch>
2017-03-06 17:34:43 +0100

    Electron: enable live reloading in dev mode

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 import { app, BrowserWindow, shell } from 'electron'
+import { enableLiveReload } from 'electron-compile'
 
 /**
  * Keep a global reference of the window object, else the window will
@@ -19,6 +20,9 @@ app.on('window-all-closed', () => {
 
 /** Ready to load the application. */
 app.on('ready', () => {
+  /** Enable live reloading on code changes if in dev mode. */
+  process.env.NODE_ENV === 'dev' && enableLiveReload()
+
   mainWindow = new BrowserWindow({
     width: 1152,
     height: 700,
