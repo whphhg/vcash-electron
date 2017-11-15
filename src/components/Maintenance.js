dee1e11801fc241aa44ec640356a42208336227f
whphhg <whphhg@protonmail.ch>
2017-11-15 16:01:18 +0100

    Maintenance: refactor and update to new layout

--- src/components/Maintenance.js
+++ src/components/Maintenance.js
@@ -1,15 +1,20 @@
 import React from 'react'
 import { translate } from 'react-i18next'
+import { Route, withRouter } from 'react-router-dom'
+import { action, extendObservable } from 'mobx'
 import { inject, observer } from 'mobx-react'
-import moment from 'moment'
 
 /** Ant Design */
-import Table from 'antd/lib/table'
+import Menu from 'antd/lib/menu'
 
-/** Required components. */
-import SelectCurrency from './SelectCurrency'
-import SelectLanguage from './SelectLanguage'
-import SelectSoundAlerts from './SelectSoundAlerts'
+/** Components */
+import Console from './Console'
+import Header from './Header'
+import Footer from './Footer'
+import PrivateKeyDump from './PrivateKeyDump'
+import PrivateKeyImport from './PrivateKeyImport'
+import SetLocaleSettings from './SetLocaleSettings'
+import SetSoundAlerts from './SetSoundAlerts'
 import WalletBackup from './WalletBackup'
 import WalletDump from './WalletDump'
 import WalletEncrypt from './WalletEncrypt'
@@ -17,126 +22,139 @@ import WalletPassphraseChange from './WalletPassphraseChange'
 import WalletRepair from './WalletRepair'
 import WalletSeedDump from './WalletSeedDump'
 
-@translate(['wallet'], { wait: true })
-@inject('gui', 'walletNext')
+@translate(['wallet'])
+@inject('connections', 'gui', 'wallet')
 @observer
 class Maintenance extends React.Component {
   constructor(props) {
     super(props)
     this.t = props.t
+    this.connections = props.connections
     this.gui = props.gui
-    this.wallet = props.walletNext
+    this.wallet = props.wallet
+
+    /** Extend the component with observable properties. */
+    extendObservable(this, {
+      selected: '/' + this.connections.viewingId + '/maintenance/general'
+    })
+  }
+
+  /**
+   * Set selected menu item and navigate to it.
+   * @function setSelected
+   * @param {object} item - Routing path and menu item key.
+   */
+  @action
+  setSelected = ({ key }) => {
+    this.selected = key
+
+    if (this.props.history.location.pathname !== key) {
+      this.props.history.push(key)
+    }
   }
 
   render() {
+    const key = '/' + this.connections.viewingId + '/maintenance/'
+
     return (
-      <div id="MaintenanceGrid">
-        <div className="shadow">
-          <div style={{ margin: '0 10px 0 10px' }}>
-            <div className="flex-sb" style={{ minHeight: '32px' }}>
-              <div className="flex">
-                <i className="material-icons md-16">language</i>
-                <p style={{ margin: '0 10px 0 5px' }}>
-                  {this.t('wallet:selectLanguage')}
-                </p>
-                <SelectLanguage />
-              </div>
-              <div className="flex">
-                <i className="material-icons md-16">monetization_on</i>
-                <p style={{ margin: '0 10px 0 5px' }}>
-                  {this.t('wallet:selectCurrency')}
-                </p>
-                <SelectCurrency />
-              </div>
+      <div id="AppListContent">
+        <div className="list grid-tb">
+          <div style={{ width: '100%' }}>
+            <Menu
+              mode="inline"
+              onClick={this.setSelected}
+              selectedKeys={[this.selected]}
+            >
+              <Menu.Item key={key + 'general'}>
+                <div className="flex">
+                  <i className="material-icons md-20">tune</i>
+                  <p style={{ margin: '0 0 0 10px' }}>
+                    {this.t('maintenance')}
+                  </p>
+                </div>
+              </Menu.Item>
+              <Menu.Item key={key + 'dump'}>
+                <div className="flex">
+                  <i className="material-icons md-20">arrow_upward</i>
+                  <p style={{ margin: '0 0 0 10px' }}>
+                    {this.t('maintenanceDump')}
+                  </p>
+                </div>
+              </Menu.Item>
+              <Menu.Item key={key + 'import'}>
+                <div className="flex">
+                  <i className="material-icons md-20">arrow_downward</i>
+                  <p style={{ margin: '0 0 0 10px' }}>
+                    {this.t('maintenanceImport')}
+                  </p>
+                </div>
+              </Menu.Item>
+              <Menu.Item key={key + 'console'}>
+                <div className="flex">
+                  <i className="material-icons md-20">remove_from_queue</i>
+                  <p style={{ margin: '0 0 0 10px' }}>{this.t('rpcConsole')}</p>
+                </div>
+              </Menu.Item>
+            </Menu>
+          </div>
+          <div style={{ width: '100%' }}>
+            <div style={{ padding: '0 10px' }}>
+              <SetSoundAlerts />
+            </div>
+            <hr />
+            <div style={{ padding: '0 10px 10px 10px' }}>
+              <SetLocaleSettings />
             </div>
           </div>
         </div>
-        <div className="shadow">
-          <div style={{ display: 'flex', margin: '10px' }}>
-            <div style={{ flex: 1, margin: '0 10px 0 0' }}>
-              <WalletBackup />
-              <hr />
-              <WalletDump />
-              <hr />
-              <WalletSeedDump />
-            </div>
-            <div style={{ flex: 1, margin: '0 0 0 10px' }}>
-              <WalletEncrypt />
-              <WalletPassphraseChange />
-              <hr />
-              <WalletRepair />
-              <hr />
-              <SelectSoundAlerts />
-            </div>
-          </div>
+        <div id="AppHeaderFooter">
+          <Header />
+          <Route
+            path={key + 'general'}
+            render={() => (
+              <div>
+                <WalletEncrypt />
+                <WalletPassphraseChange />
+                <hr />
+                <WalletBackup />
+                <hr />
+                <WalletRepair />
+              </div>
+            )}
+          />
+          <Route
+            path={key + 'dump'}
+            render={() => (
+              <div>
+                <PrivateKeyDump />
+                <hr />
+                <WalletDump />
+                <hr />
+                <WalletSeedDump />
+              </div>
+            )}
+          />
+          <Route
+            path={key + 'import'}
+            render={() => (
+              <div>
+                <PrivateKeyImport />
+              </div>
+            )}
+          />
+          <Route
+            path={key + 'console'}
+            render={() => (
+              <div style={{ padding: '10px 10px 0 11px' }}>
+                <Console />
+              </div>
+            )}
+          />
+          <Footer />
         </div>
-        <Table
-          bordered
-          columns={[
-            {
-              dataIndex: 'ip',
-              title: this.t('wallet:peers'),
-              width: 120
-            },
-            {
-              dataIndex: 'port',
-              title: this.t('wallet:port'),
-              width: 90
-            },
-            {
-              dataIndex: 'version',
-              title: this.t('wallet:version'),
-              width: 90
-            },
-            {
-              dataIndex: 'os',
-              title: this.t('wallet:os'),
-              width: 110
-            },
-            {
-              dataIndex: 'inbound',
-              title: this.t('wallet:inbound'),
-              width: 110,
-              render: (text, record) =>
-                record.inbound === true
-                  ? this.t('wallet:yes')
-                  : this.t('wallet:no')
-            },
-            {
-              dataIndex: 'conntime',
-              title: this.t('wallet:connected'),
-              width: 170,
-              render: text => moment(text * 1000).fromNow()
-            },
-            {
-              dataIndex: 'lastrecv',
-              title: this.t('wallet:lastReceived'),
-              width: 170,
-              render: text => moment(text * 1000).fromNow()
-            },
-            {
-              dataIndex: 'startingheight',
-              title: this.t('wallet:startingHeight'),
-              width: 130,
-              render: text =>
-                new Intl.NumberFormat(this.gui.language).format(text)
-            },
-            {
-              dataIndex: 'banscore',
-              title: this.t('wallet:banScore'),
-              render: text => text + '/100'
-            }
-          ]}
-          dataSource={this.wallet.peers}
-          locale={{ emptyText: this.t('wallet:notFound') }}
-          pagination={false}
-          scroll={this.wallet.peers.length > 8 ? { y: 158 } : {}}
-          size="small"
-          style={{ margin: '10px 10px 0 10px' }}
-        />
       </div>
     )
   }
 }
 
-export default Maintenance
+export default withRouter(Maintenance)
