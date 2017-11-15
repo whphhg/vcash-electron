a23c6b05d0f248d03ac753d68211a5ca75c8e70f
whphhg <whphhg@protonmail.ch>
2017-11-15 17:29:36 +0100

    RPC: cleanup and correctly return empty object on invalid response

--- src/stores/rpc.js
+++ src/stores/rpc.js
@@ -1,40 +1,42 @@
 import { computed, reaction } from 'mobx'
+import rpcs from '../utilities/rpcs'
 
-export default class RPC {
+class RPC {
   /**
-   * @constructor
-   * @param {object} connection - Connection settings and status.
-   * @param {function} setStatus - Set connection status.
-   * @property {number|null} testTimeout - testExecute() timeout id.
+   * @param {object} connection - Connection instance.
+   * @prop {array} methods - RPC method names.
+   * @prop {number|null} testTimeout - testRPC() timeout id.
    */
-  constructor(connection, setStatus) {
+  constructor(connection) {
     this.connection = connection
-    this.setStatus = setStatus
+    this.methods = Object.keys(rpcs)
     this.testTimeout = null
 
-    /** Test execute() on start-up. */
+    /** Assign camelCase named RPC methods as functions wrapping batch(). */
+    Object.keys(rpcs).forEach(method => {
+      this[rpcs[method]] = async function() {
+        const res = await this.batch([{ method, params: [...arguments] }], true)
+        return res
+      }
+    })
+
+    /** Test RPC on local connection start or clear testTimeout on stop. */
     reaction(
       () => this.connection.status.active,
       active => {
-        if (active === true && this.connection.type === 'local') {
-          this.testExecute()
-        }
-
-        /** Clear timeout on shut-down. */
-        if (active === false) {
-          clearTimeout(this.testTimeout)
-        }
-      }
+        if (active === true && this.connection.type === 'local') this.testRPC()
+        if (active === false) clearTimeout(this.testTimeout)
+      },
+      { name: 'RPC: checking if connection started or stopped.' }
     )
 
-    /** Test execute() on SSH tunnel ready. */
+    /** Test RPC on SSH tunnel ready. */
     reaction(
       () => this.connection.status.tunnel,
       tunnel => {
-        if (tunnel === true) {
-          this.testExecute()
-        }
-      }
+        if (tunnel === true) this.testRPC()
+      },
+      { name: 'RPC: checking if SSH tunnel started or stopped.' }
     )
   }
 
@@ -50,60 +52,68 @@ export default class RPC {
   }
 
   /**
-   * Execute RPC request(s).
-   * @function execute
-   * @param {array} options - RPC request objects.
-   * @param {function} callback - Callback for RPC response.
+   * Make a single RPC request or a batch of RPC requests.
+   * @function batch
+   * @param {array} req - RPC method and params object(s).
+   * @param {string} single - Return single response if only 1 RPC request.
+   * @returns {object} Response(s) and request(s), single response, or empty.
    */
-  execute(options, callback) {
-    /** Add jsonrpc version and random id to each option. */
-    options.map(option => {
-      option.jsonrpc = '2.0'
-      option.id = Math.floor(Math.random() * 10000)
-    })
+  async batch(req, single = false) {
+    try {
+      /** Add jsonrpc version and random id to each RPC request. */
+      req.map(rpc => {
+        rpc.jsonrpc = '2.0'
+        rpc.id = Math.floor(Math.random() * 10000)
+      })
 
-    window
-      .fetch('http://127.0.0.1:' + this.connection.localPort, {
-        method: 'POST',
-        headers: {
-          Accept: 'application/json',
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(options)
-      })
-      .then(response => {
-        if (response.ok === true) return response.json()
-      })
-      .then(data => {
-        /** Update connection status. */
-        if (this.connection.status.rpc !== true) {
-          this.setStatus(this.connection.uid, { rpc: true })
+      /** Serialize the RPC request(s) and make the POST request. */
+      const request = await window.fetch(
+        'http://127.0.0.1:' + this.connection.localPort,
+        {
+          method: 'POST',
+          body: JSON.stringify(req)
         }
+      )
 
-        return callback(data, options)
-      })
-      .catch(error => {
-        console.error('RPC:', options, error.message)
+      /** Get the JSON response(s). */
+      const res = await request.json()
 
-        /** Reject invalid responses, but don't alter connection status. */
-        if (error.message.indexOf('Unexpected token') !== -1) return
+      /** Toggle connection RPC status if not true. */
+      if (this.connection.status.rpc !== true) {
+        this.connection.setStatus({ rpc: true })
+      }
 
-        /** Update connection status. */
-        if (this.connection.status.rpc !== false || this.testTimeout === null) {
-          this.setStatus(this.connection.uid, { rpc: false })
-        }
+      /** Return a single response, or response(s) and request(s). */
+      if (res.length === 1 && single === true) return res[0]
+      return { res, req }
+    } catch (error) {
+      console.error('rpc.batch:', error.message)
 
-        /** Test execute every 5s until daemon is reachable. */
-        this.testTimeout = setTimeout(() => this.testExecute(), 5 * 1000)
-      })
+      /** Reject invalid responses without altering the RPC status. */
+      if (error.message.indexOf('Unexpected token') !== -1) return {}
+
+      /** Toggle connection RPC status if not false. */
+      if (this.connection.status.rpc !== false || this.testTimeout === null) {
+        this.connection.setStatus({ rpc: false })
+      }
+
+      /** Test RPC every 5s until the daemon is connectable. */
+      this.testTimeout = setTimeout(() => this.testRPC(), 5 * 1000)
+
+      /** Return empty object on a failed request. */
+      return {}
+    }
   }
 
   /**
-   * Clear previous timeout id and test execute() using RPC getinfo.
-   * @function testExecute
+   * Test daemon RPC connectivity.
+   * @function testRPC
    */
-  testExecute() {
+  async testRPC() {
     clearTimeout(this.testTimeout)
-    this.execute([{ method: 'getinfo', params: [] }], response => {})
+    await this.getInfo()
   }
 }
+
+/** Export store class as default export. */
+export default RPC
