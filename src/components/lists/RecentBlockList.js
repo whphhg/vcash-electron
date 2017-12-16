7b5b28c11dd4cc0f7d02f04cd5c22ce5c7d2243f
whphhg <whphhg@protonmail.ch>
2017-12-16 13:34:12 +0100

    Lists: add RecentBlockList

---
+++ src/components/lists/RecentBlockList.js
@@ -0,0 +1,51 @@
+import React from 'react'
+import List from 'react-list'
+import { translate } from 'react-i18next'
+import { inject, observer } from 'mobx-react'
+
+/** Component */
+import RecentBlockListItem from './RecentBlockListItem'
+
+@translate(['wallet'])
+@inject('gui', 'statistics')
+@observer
+class RecentBlockList extends React.Component {
+  constructor(props) {
+    super(props)
+    this.t = props.t
+    this.gui = props.gui
+    this.statistics = props.statistics
+  }
+
+  render() {
+    return (
+      <div>
+        <div className="flex list-header">
+          <p style={{ minWidth: '65px' }}>{this.t('block')}</p>
+          <p style={{ minWidth: '50px' }}>{this.t('type')}</p>
+          <p style={{ minWidth: '130px' }}>{this.t('age')}</p>
+          <p style={{ minWidth: '80px' }}>{this.t('size')}</p>
+          <p style={{ minWidth: '80px' }}>{this.t('txs')}</p>
+        </div>
+        <div
+          className="list-plain"
+          style={{ maxHeight: this.gui.window.height - 347 }}
+        >
+          <List
+            length={this.statistics.recentBlocks.length}
+            itemRenderer={(index, key) => (
+              <RecentBlockListItem
+                index={index}
+                key={key}
+                gui={this.gui}
+                statistics={this.statistics}
+              />
+            )}
+          />
+        </div>
+      </div>
+    )
+  }
+}
+
+export default RecentBlockList
---
+++ src/components/lists/RecentBlockListItem.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import { observer } from 'mobx-react'
+import { humanReadable } from '../../utilities/common'
+import moment from 'moment'
+
+const RecentBlockListItem = observer(props => {
+  const block = props.statistics.recentBlocks[props.index]
+  return (
+    <div className={'list-item-plain' + (props.index % 2 === 0 ? ' even' : '')}>
+      <div className="flex">
+        <p style={{ minWidth: '65px' }}>{block.height}</p>
+        <p style={{ minWidth: '50px' }}>{block.type}</p>
+        <p style={{ minWidth: '130px' }}>{moment(block.time).fromNow(true)}</p>
+        <p style={{ minWidth: '80px' }}>
+          {humanReadable(block.size, true, 'B', props.gui.language)}
+        </p>
+        <p style={{ minWidth: '80px' }}>{block.txCount}</p>
+      </div>
+    </div>
+  )
+})
+
+export default RecentBlockListItem
